1bfddd7ecc50dbd79ca304129057f49148bcd2f1
IlProfColazzo <72198473+IlProfColazzo@users.noreply.github.com>
2020-10-19 13:08:56 +0200

    Revert "Risoluzione"
    This reverts commit 946e7ffc6e0c9c0c9171b511f58e06c06246ef66.

--- GestioneDipendenti/src/Dipendente.java
+++
@@ -1,9 +0,0 @@
-
-public class Dipendente extends Persona{
-
-	public Dipendente(String nome, String numero, String indirizzo, String CF, float paga) {
-		super(nome, numero, indirizzo, CF,paga);
-		// TODO Auto-generated constructor stub
-	}
-
-}
--- GestioneDipendenti/src/Volontario.java
+++
@@ -1,9 +0,0 @@
-
-public class Volontario extends Persona{
-
-	public Volontario(String nome, String numero, String indirizzo, String CF) {
-		super(nome, numero, indirizzo, CF,0);
-		// TODO Auto-generated constructor stub
-	}
-
-}
